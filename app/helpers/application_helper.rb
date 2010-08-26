c4d0d140b278e2d2f1f5d637bcd22fd2d37a2d45
Kerry Buckley <kerryjbuckley@gmail.com>
2010-08-26 23:40:09 +0100

    Rollover and current page highlights for star links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       "Byte Night Bedtime Story"
     end
   end
+
+  def link_to_home
+    link_class = (controller.controller_name == "home") ? "home_active" : "home_inactive"
+    %(<li><a href="/" id="#{link_class}"><span>Home</span></a>)
+  end
+
+  def link_to_page page, label
+    link_class = if controller.controller_name == "pages" && controller.params[:page] == page
+                   "#{page}_active"
+                 else
+                   "#{page}_inactive"
+                 end
+    %(<li><a href="/#{page}" id="#{link_class}"><span>#{label}</span></a>)
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,4 +15,58 @@ describe ApplicationHelper do
       end
     end
   end
+
+  describe "generating the link to the home page" do
+    before do
+      @controller = mock
+      helper.stub(:controller).and_return @controller
+    end
+
+    context "when on the home page" do
+      before do
+        @controller.stub(:controller_name).and_return "home"
+      end
+
+      it "generates a link with an id of 'home_active'" do
+        helper.link_to_home.should == '<li><a href="/" id="home_active"><span>Home</span></a>'
+      end
+    end
+
+    context "when not on the home page" do
+      before do
+        @controller.stub(:controller_name).and_return "foo"
+      end
+
+      it "generates a link with an id of 'home_inactive'" do
+        helper.link_to_home.should == '<li><a href="/" id="home_inactive"><span>Home</span></a>'
+      end
+    end
+  end
+
+  describe "generating the link to a page" do
+    before do
+      @controller = mock :controller_name => "pages"
+      helper.stub(:controller).and_return @controller
+    end
+
+    context "when on that page" do
+      before do
+        @controller.stub(:params).and_return({:page => "foo"})
+      end
+
+      it "generates a link with an id of '<page>_active'" do
+        helper.link_to_page("foo", "Foo Bar").should == '<li><a href="/foo" id="foo_active"><span>Foo Bar</span></a>'
+      end
+    end
+
+    context "when on another page" do
+      before do
+        @controller.stub(:params).and_return({:page => "bar"})
+      end
+
+      it "generates a link with an id of '<page>_inactive'" do
+        helper.link_to_page("foo", "Foo Bar").should == '<li><a href="/foo" id="foo_inactive"><span>Foo Bar</span></a>'
+      end
+    end
+  end
 end
