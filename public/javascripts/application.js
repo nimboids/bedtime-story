d09eadd760c7f02e83b4ef9f540ec10646b1e840
Kerry Buckley <kerryjbuckley@gmail.com>
2010-09-01 08:54:04 +0100

    Refactor javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
+  scrollToEndOfStory();
+  $("#story_contribution_text").keyup(updateCharactersRemaining);
+  $("#story_contribution_text").focus();
+});
+
+function scrollToEndOfStory() {
   var storyDiv = $("#story");
   if (storyDiv != null) {
     storyDiv.scrollTop(storyDiv[0].scrollHeight);
   }
-  $("#story_contribution_text").keyup(update_characters_remaining);
-  $("#story_contribution_text").focus();
-});
+}
 
-function update_characters_remaining(){
+function updateCharactersRemaining(){
   $("#characters_remaining").html((140 - $("#story_contribution_text")[0].value.length) +
       " characters remaining");
 }
