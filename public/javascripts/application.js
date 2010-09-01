053ab0129054214f1c64b727f2624eab0fd25ab9
Kerry Buckley <kerryjbuckley@gmail.com>
2010-09-01 07:50:32 +0100

    Add jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-window.onload=function(){
-  var storyDiv = document.getElementById("story");
+$(document).ready(function(){
+  var storyDiv = $("#story");
   if (storyDiv != null) {
-    storyDiv.scrollTop = storyDiv.scrollHeight;
+    storyDiv.scrollTop(storyDiv[0].scrollHeight);
   }
-  var contributionTextarea = document.getElementById("story_contribution_text");
-  if (contributionTextarea != null) {
-    contributionTextarea.focus();
-  }
-}
+  $("#story_contribution_text").focus();
+});
+
