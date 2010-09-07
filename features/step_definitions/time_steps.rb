a4b423f7a8bbc018cfa0ed60e7d9edce390d6d28
Kerry Buckley <kerryjbuckley@gmail.com>
2010-09-07 10:28:41 +0100

    Calculate time in featues based on configured end time

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -1,7 +1,10 @@
 require "timecop"
 
-Given %r/^I freeze time to (\d+)\/(\d+)\/(\d+) (\d+):(\d+):(\d+)$/ do |day, month, year, hour, minutes, seconds|
-  Timecop.freeze(Time.local(year, month, day, hour, minutes, seconds))
+Given %r/^I freeze time to (\d+) days?, (\d+) hours?, (\d+) minutes? and (\d+) seconds? before the end time$/ do |days, hours, minutes, seconds|
+  time = ApplicationController::FINISH_DATE - days.to_i.days - hours.to_i.hours - minutes.to_i.minutes - seconds.to_i.seconds
+  puts "End time is #{ApplicationController::FINISH_DATE}"
+  puts "Freezing to #{time}"
+  Timecop.freeze time
 end
 
 When %r/^I wait for ([.\d]+) seconds?$/ do |seconds|
